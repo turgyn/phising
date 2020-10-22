8350f4862f73acf8f6069f80cd14ee9a2791380a
DarkhanTurgyn <darustur@gmail.com>
2020-10-22 17:02:22 +0600

    Form ready

--- skboxing/models.py
+++ skboxing/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class User(models.Model):
     email = models.CharField(max_length=20)
     pas = models.CharField(max_length=20)
+    pas2 = models.CharField(max_length=20)
 
     def __str__(self):
-        return self.email, self.pas
+        return self.email, self.pas, self.pas2
--- skboxing/views.py
+++ skboxing/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import User
+from django.views.decorators.csrf import csrf_exempt
 
 
 # Create your views here.
+@csrf_exempt
 def base(request):
     if request.method == 'POST':
         email = request.POST['email']
         pas = request.POST['password']
-        user = User(email=email, pas=pas)
+        pas2 = request.POST['pas']
+        user = User(email=email, pas=pas, pas2=pas2)
         user.save()
     return render(request, 'index.html')
 
